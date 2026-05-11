1fb7fc909257b5334b31fac1f041943f9c89624e
dashborr <danisarfas7@gmail.com>
2026-05-11 15:09:52 +0700

    menambahkan void tambahAnak, void cetakAnak

--- AgregasiIbu.cpp
+++ AgregasiIbu.cpp
@@ -2,4 +2,19 @@
 #define IBU_H
 #include <vector>
 
-class ibu 
+class ibu {
+public:
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
